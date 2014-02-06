761244e9216a32f215041771b05a914a7cb64cec
CrashOveride2010 <danielmires@yahoo.com>
2014-02-06 12:14:34 -0800

    Java Assignment 1.1
    page 6

---
+++ Assignment 1.1.java
@@ -0,0 +1,15 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello reader.");
+System.out.println("Welcome to Java.");
+
+System.out.println("Lets demonstrate a simple calculation.");
+int answer;
+answer = 2 + 2;
+System.out.println("2 plus 2 is " + answer);
+	}
+
+}
